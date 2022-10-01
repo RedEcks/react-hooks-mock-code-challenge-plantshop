33b02cc6235679fb8886f27a21cc664f56511fcc
Xavier Dunkley <xavierxv18@gmail.com>
2022-10-01 17:49:45 -0400

    rendered names of artists

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
+//1. Render plants. Use map to render different plant links. Use useEffect and fetch 
+//2. Add new plant by POST. Need to manage form using State
+//3. Use state to mark plant as sold out using State 
+//4. Use filter to find plants by name 
+
 function App() {
+  const [plantInfo,setplantsInfo] = useState([])
+
+  useEffect(()=>{
+    fetch("http://localhost:6001/plants")
+      .then((response)=>response.json())
+      .then((data)=>setplantsInfo(data))
+  },[])
+
   return (
     <div className="app">
       <Header />
-      <PlantPage />
+      <PlantPage plantInfo={plantInfo} />
     </div>
   );
 }
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,12 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({plantInfo}) {
+
+  let plants = plantInfo.map((plant)=><PlantCard plant={plant}/>)
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{plants}</ul>
   );
 }
 
